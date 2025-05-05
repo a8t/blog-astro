e83fcda299ed3b3561d23854360f7d76392c6847
Andy Tran <andy8tran@gmail.com>
2025-05-05 00:58:20 -0400

    remove gitignore package-lock

--- src/lib/components/chords/Chords.tsx
+++ src/lib/components/chords/Chords.tsx
@@ -11,7 +11,6 @@ import {
 } from "./chordStore";
 
 import { getChordStoreKey, stringifyKey } from "./chordStoreKey";
-import { getConstantValue } from "typescript";
 
 export function TransposeControl() {
   const chordStoreKey = getChordStoreKey();
